179a119055d2f6b877ba45ec997a87ae84da4f7b
DmytroDemianchuk <moqitee@gmail.com>
2023-12-18 11:24:57 +0100

    added 2 button to open xlsx files

--- main.go
+++ main.go
@@ -2,71 +2,115 @@ package main
 
 import (
 	"fmt"
+	"io"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/xuri/excelize/v2"
 )
 
+func createTable(data [][]string) fyne.CanvasObject {
+	rows := len(data)
+	cols := 0
+	if rows > 0 {
+		cols = len(data[0])
+	}
+
+	table := widget.NewTable(
+		func() (int, int) {
+			return rows, cols
+		},
+		func() fyne.CanvasObject {
+			label := widget.NewLabel("")
+			return label
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			if i.Row < len(data) && i.Col < len(data[i.Row]) {
+				label, ok := o.(*widget.Label)
+				if ok {
+					label.SetText(data[i.Row][i.Col])
+				}
+			}
+		},
+	)
+
+	for i := 0; i < cols; i++ {
+		table.SetColumnWidth(i, 150)
+	}
+	for i := 0; i < rows; i++ {
+		table.SetRowHeight(i, 30)
+	}
+	table.Refresh()
+
+	scrollableTable := container.NewScroll(table)
+
+	return scrollableTable
+}
+
 func main() {
 	myApp := app.New()
 
-	// Create a window
-	myWindow := myApp.NewWindow("XLSX Reader")
+	myWindow := myApp.NewWindow("Resizable & Movable Table")
+	myWindow.Resize(fyne.NewSize(800, 600))
 
-	// Create a label to display the content
-	contentLabel := widget.NewLabel("Select an XLSX file to display its content")
+	updateContent := func(data [][]string) {
+		tableContent := createTable(data)
+		tableWindow := myApp.NewWindow("Table Content")
+		tableWindow.Resize(fyne.NewSize(800, 600))
+		tableWindow.SetContent(tableContent)
+		tableWindow.Show()
+	}
 
-	// Create a button to open the file dialog
-	openButton := widget.NewButton("Open File", func() {
-		fileDialog := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			if reader == nil {
+	openFile := func(reader io.ReadCloser) {
+		f, err := excelize.OpenReader(reader)
+		if err != nil {
+			dialog.ShowError(err, myWindow)
+			return
+		}
+
+		rows, err := f.GetRows(f.GetSheetList()[0])
+		if err != nil {
+			dialog.ShowError(err, myWindow)
+			return
+		}
+
+		updateContent(rows)
+	}
+
+	openFileButton := widget.NewButton("Open File 1", func() {
+		dialog.ShowFileOpen(func(reader fyne.URIReadCloser, err error) {
+			if err != nil || reader == nil {
+				fmt.Println("Error:", err)
 				return
 			}
 			defer reader.Close()
 
-			// Read the content of the selected XLSX file
-			f, err := excelize.OpenReader(reader)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			// Get all the rows in the first sheet
-			rows, err := f.GetRows(f.GetSheetList()[0])
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			// Display the content in the label
-			var content string
-			for _, row := range rows {
-				content += fmt.Sprintf("%v\n", row)
-			}
-			contentLabel.SetText(content)
+			openFile(reader)
 		}, myWindow)
-
-		// Show the file dialog
-		fileDialog.Show()
 	})
 
-	// Create a container to hold the widgets
-	content := container.NewVBox(
-		openButton,
-		contentLabel,
+	openFileButton2 := widget.NewButton("Open File 2", func() {
+		dialog.ShowFileOpen(func(reader fyne.URIReadCloser, err error) {
+			if err != nil || reader == nil {
+				fmt.Println("Error:", err)
+				return
+			}
+			defer reader.Close()
+
+			openFile(reader)
+		}, myWindow)
+	})
+
+	initialContent := fyne.NewContainerWithLayout(layout.NewVBoxLayout(),
+		openFileButton,
+		openFileButton2,
+		widget.NewLabel("Select an XLSX file to open..."),
 	)
+	myWindow.SetContent(initialContent)
 
-	// Set the content of the window
-	myWindow.SetContent(content)
-
-	// Show and run the application
 	myWindow.ShowAndRun()
 }
